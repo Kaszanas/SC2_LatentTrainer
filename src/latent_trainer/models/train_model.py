1af6fc230012dd16454bbc74d70ee70f6b75150a
Kaszanas <andrzej.bialecki94@gmail.com>
2023-07-01 19:58:07 +0200

    Drafted code for data paths

--- src/latent_trainer/models/train_model.py
+++ src/latent_trainer/models/train_model.py
@@ -1,4 +1,16 @@
 from sc2_datasets.lightning.sc2_egset_datamodule import SC2EGSetDataModule
 
+from latent_trainer.config import LOGGING_FORMAT
+
+import logging
+
+from pathlib import Path
+
 if __name__ == "__main__":
-    print("hello world")
+    logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
+
+    download_path = Path("./data/download").resolve().as_posix()
+    unpack_path = Path("./data/unpack").resolve().as_posix()
+
+    logging.info(f"{download_path=}")
+    logging.info(f"{unpack_path=}")
