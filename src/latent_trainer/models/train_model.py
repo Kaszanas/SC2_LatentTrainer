42fd94496327a120c958e95c8a24a941c2d95591
Kaszanas <andrzej.bialecki94@gmail.com>
2024-02-24 00:52:46 +0100

    Added Guided VAE model

--- src/latent_trainer/models/train_model.py
+++ src/latent_trainer/models/train_model.py
@@ -5,6 +5,8 @@ from pathlib import Path
 
 from latent_trainer.config import LOGGING_FORMAT
 
+from sc2_datasets.torch.sc2_egset_dataset import SC2EGSetDataset
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
 
@@ -13,3 +15,5 @@ if __name__ == "__main__":
 
     logging.info(f"{download_path=}")
     logging.info(f"{unpack_path=}")
+
+    dataset = SC2EGSetDataset(download_dir=download_path, unpack_dir=unpack_path)
